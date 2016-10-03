956b010d9eabee1204d06f3741a8c391bdd850f7
Miguel Guerra <migueclase2@gmail.com>
2016-10-03 12:38:01 +0200

    Nombre con Edad y Localidad

--- Desktop/Programación/Mi nombre/MiguelRG.java
+++ Desktop/Programación/Mi nombre/MiguelRG.java
@@ -3,5 +3,6 @@ public class MiguelRG {
 public static void main(String args[]){
 	System.out.println("Miguel Rodríguez Guerra!!");
 	System.out.println("Tengo 18 años");
+	System.out.println("Nací en Villablino, León");
 	}
 }
